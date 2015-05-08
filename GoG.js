779b150300d1166fdf481353acad506c10e56348
austingcarr@gmail.com <austingcarr@gmail.com>
2015-05-07 17:20:19 -0700

    initial commit

---
+++ GoG.js
@@ -0,0 +1,40 @@
+
+//Saying Hello
+<!--incorect --> var greet = 'hello';
+<!--correct --> function sayHello(){
+                    var hi = 'hello';
+                    console.log(hi);
+                }
+
+//Bashi Quest
+var bashi = 'Now I am become Death, the Destroyer of Worlds';
+
+function becomeDeath(){
+  console.log(bashi);
+    }
+
+var grave = bashi.toUpperCase();
+
+  function graveDeath(){
+    console.log(grave)
+  }
+
+//the primitive Gnome
+var n = null;
+var num = 666;
+var str = "beast";
+var enigma = undefined;
+var bool = false;
+
+//a Kargaar's Cloak
+var cloak = (function trasure() {
+    var plunder = 13;
+    return function treasre() {return plunder += 1;}
+})();
+
+cloak();
+cloak();
+cloak();
+//having stolen the Gnomish cloak, Gryyu was able to plunder the Gnome's treasure from within, as he could see their treasure, but they could not see him
+
+
